9aaf394d59995af2d884a1245c8b3495139bf4ed
Sandipan80 <bhullarsandipan@gmail.com>
2026-05-15 12:30:03 +0530

    First

---
+++ src/PublicPages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Layout/Navbar'
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
