4998855f9ee2de9e37b97eb08a29bcb94e779a0e
Sandipan80 <bhullarsandipan@gmail.com>
2026-05-16 22:27:28 +0530

    Add Login component with basic form structure

---
+++ src/PublicPages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <form action="Login">
+        <form action="" className="item"></form>
+      </form>
+    </div>
+  )
+}
+
+export default Login
